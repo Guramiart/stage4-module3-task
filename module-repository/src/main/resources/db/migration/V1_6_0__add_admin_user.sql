2c0f587a7d4f300811ad4f6a8bf1f0438f1dc10f
Artem Pasko <guramibox777@gmail.com>
2023-10-20 00:10:33 +0300

    fix sql file

--- module-repository/src/main/resources/db/migration/V1_6_0__add_admin_user.sql
+++ module-repository/src/main/resources/db/migration/V1_6_0__add_admin_user.sql
@@ -1,5 +1,4 @@
-INSERT INTO roles (Id, Name) VALUES(default, 'ROLE_ADMIN') ON CONFLICT (Name) DO NOTHING;
+INSERT INTO roles (Id, Name) VALUES (default, 'ROLE_ADMIN');
 INSERT INTO users (Id, Username, Password)
-    VALUES(default, 'admin', '$2a$10$BppoGW1vDnY0o51QzTwMDe9KYiZ5AS5Ou9lFDQgDiHSqTdBFv0hFi')
-    ON CONFLICT (Username) DO NOTHING;
+    VALUES (default, 'admin', '$2a$10$BppoGW1vDnY0o51QzTwMDe9KYiZ5AS5Ou9lFDQgDiHSqTdBFv0hFi');
 INSERT INTO users_roles (User_Id, Role_Id) VALUES(1, 1);
