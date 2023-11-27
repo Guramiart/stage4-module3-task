6d73fb293627ac9e9a58d10a9f109e355057ab3e
Artem Pasko <guramibox777@gmail.com>
2023-11-27 15:19:41 +0300

    update cors

--- module-web/src/main/java/com/mjc/school/web/configuration/SecurityConfig.java
+++ module-web/src/main/java/com/mjc/school/web/configuration/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends GlobalMethodSecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf(csrf ->
                         csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/api/**")))
                 .authorizeHttpRequests()
